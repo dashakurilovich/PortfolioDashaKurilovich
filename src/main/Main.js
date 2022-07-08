85f8f0bc7e3dd2f42e6d92009f96092615eee83f
dashakurilovich <Dasha23_@mail.ru>
2022-07-09 01:27:49 +0300

    particles

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 import styles from './Main.module.scss';
-import styleContainer from '../common/styles/Conteiner.module.css'
+import particlesSpace from '../assets/particles/space.json';
 
 function MainBlock() {
-    return (
-        <div className={styles.mainBlock}>
-            <div className={styles.container}>
-                <div className={styles.text}>
-                    <span>Hi There</span>
-                    <span> I am  <span>Dasha Kurilovich</span></span>
-                    <h1>Frontend Developer.</h1>
-                </div>
-                <div className={styles.photo}>
-                    <div className={styles.image}></div>
-                </div>
-            </div>
-        </div >
-    );
+
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  return (
+    <div className={styles.mainBlock}>
+      <Particles init={particlesInit} options={particlesSpace} className={styles.particles} />
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <span>Hi There</span>
+          <span> I am  <span>Dasha Kurilovich</span></span>
+          <h1>Frontend Developer.</h1>
+        </div>
+        <div className={styles.photo}>
+          <div className={styles.image}></div>
+        </div>
+      </div>
+    </div >
+  );
 }
 
 export default MainBlock;
